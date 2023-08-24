ef676a8e5a075c9d2a9a1788acf732083fdf6e61
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-08-24 18:31:49 -0300

    criado teste para o feed

--- backend/src/tests/step_definitions/test_feed.py
+++ backend/src/tests/step_definitions/test_feed.py
@@ -14,19 +14,28 @@ def test_get_feed():
 def func1(context):
     pass
 
-@when(parsers.cfparse('uma requisição GET for enviada para "/feed"'))
-def func2(context):
-    context['response'] = requests.get(url + "feed/")
+@when(parsers.cfparse('uma requisição GET for enviada para "/{route}"'))
+def func2(context, route):
+    context['response'] = requests.get(url + f"{route}")
     pass
 
-@then(parsers.cfparse('o status da resposta deve ser "{200}"'))
-def func3(context, param):
+@then(parsers.cfparse('o status da resposta deve ser "{status_code}"'))
+def func3(context, status_code):
+    assert context['response'].status_code == int(status_code)
     pass
 
-@then(parsers.cfparse('o JSON da resposta deve ser uma lista composta por id'))
-def func4(context):
+@then(parsers.cfparse('o JSON da resposta deve ser uma lista composta por {key}'))
+def func4(context, key):
+    context['post_ids'] = context['response'].json()['posts']
+
+    for post_id in context['post_ids']:
+        dict_key = list(post_id.keys())[0]
+        assert dict_key == key
+
     pass
 
-@then(parsers.cfparse('o post com id "{param1}" tem posição "{param2}" na lista'))
-def func5(context):
-    pass
+@then(parsers.cfparse('o post com id "{post_id}" tem posição "{post_position}" na lista'))
+def func5(context, post_id, post_position):
+    actual_position = context['post_ids'].index({'id': int(post_id)})
+    assert actual_position == int(post_position)
+
