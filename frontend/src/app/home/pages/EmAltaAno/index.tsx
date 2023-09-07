2d41b1f521183fa4cf86c6a28c4c504219a3a030
almirAlmir <aasc@cin.ufpe.br>
2023-09-06 22:08:35 -0300

    feat(emAlta): Link rotas em alta completo

--- frontend/src/app/home/pages/EmAltaAno/index.tsx
+++ frontend/src/app/home/pages/EmAltaAno/index.tsx
@@ -5,18 +5,18 @@ import { useEffect, useRef, useState } from "react";
 
 
 
-const EmAlta = () => {
+const EmAltaAno = () => {
     const [animeList, setAnimelist] = useState([]);
 
-    const getAnimes = async() => {
+    const getAnimesAno = async() => {
 
-        const response = await axios.get('http://localhost:8000/emalta/dia');
+        const response = await axios.get('http://localhost:8000/emalta/ano');
         setAnimelist(response.data);
     }
 
     useEffect(() => {
         
-        getAnimes();
+        getAnimesAno();
 
     }, []);
 
@@ -26,10 +26,10 @@ const EmAlta = () => {
             <div>
                 <h2>Em Alta</h2>
                 <div className="btn-group">
-                    <a href="#" className="btn btn-primary active" aria-current="page">Dia</a>
+                    <a href="http://localhost:3000/emalta/dia" className="btn btn-primary">Dia</a>
                     <a href="http://localhost:3000/emalta/semana" className="btn btn-primary">Semana</a>
                     <a href="http://localhost:3000/emalta/trimestre" className="btn btn-primary">Trimestre</a>
-                    <a href="http://localhost:3000/emalta/ano" className="btn btn-primary">Ano</a>
+                    <a href="#" className="btn btn-primary active" aria-current="page">Ano</a>
                     </div>
                 <table className="table table-dark table-striped">
                     <tr>
@@ -50,4 +50,4 @@ const EmAlta = () => {
     )
 };
 
-export default EmAlta;
+export default EmAltaAno;
--- frontend/src/app/home/pages/EmAltaTrimestre/index.tsx
+++ frontend/src/app/home/pages/EmAltaTrimestre/index.tsx
@@ -5,18 +5,18 @@ import { useEffect, useRef, useState } from "react";
 
 
 
-const EmAlta = () => {
+const EmAltaTrimestre = () => {
     const [animeList, setAnimelist] = useState([]);
 
-    const getAnimes = async() => {
+    const getAnimesTrimestre = async() => {
 
-        const response = await axios.get('http://localhost:8000/emalta/dia');
+        const response = await axios.get('http://localhost:8000/emalta/trimestre');
         setAnimelist(response.data);
     }
 
     useEffect(() => {
         
-        getAnimes();
+        getAnimesTrimestre();
 
     }, []);
 
@@ -26,9 +26,9 @@ const EmAlta = () => {
             <div>
                 <h2>Em Alta</h2>
                 <div className="btn-group">
-                    <a href="#" className="btn btn-primary active" aria-current="page">Dia</a>
+                    <a href="http://localhost:3000/emalta/dia" className="btn btn-primary">Dia</a>
                     <a href="http://localhost:3000/emalta/semana" className="btn btn-primary">Semana</a>
-                    <a href="http://localhost:3000/emalta/trimestre" className="btn btn-primary">Trimestre</a>
+                    <a href="#" className="btn btn-primary active" aria-current="page">Trimestre</a>
                     <a href="http://localhost:3000/emalta/ano" className="btn btn-primary">Ano</a>
                     </div>
                 <table className="table table-dark table-striped">
@@ -50,4 +50,4 @@ const EmAlta = () => {
     )
 };
 
-export default EmAlta;
+export default EmAltaTrimestre;
