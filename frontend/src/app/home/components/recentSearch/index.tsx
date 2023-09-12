a412edae8a1af2ec0fb045a5bd6ab49e118b90d5
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-09-12 20:31:21 -0300

    botões adicionados à barra de busca

---
+++ frontend/src/app/home/components/recentSearch/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./index.module.css";
+
+const recentSearch = () => {
+
+    /*const side_bar = () => {
+        window.alert("abrir sidebar");
+    }*/
+
+    
+  return (
+    <section className={styles.container}>
+        <button></button>
+    </section>
+  );
+};
+
+export default recentSearch;
