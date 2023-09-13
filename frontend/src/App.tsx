de7bf4075bce5a5fd66e8af3db1193945d2c8af7
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-09-13 15:07:20 -0300

    tela de busca inserida

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./app/home/components/header/index";
 import CreateTest from "./app/home/pages/CreateTest";
 import ListTests from "./app/home/pages/ListTests";
 import EmAlta from "./app/home/pages/EmAlta";
 import maisVistos from "./app/home/pages/maisVistos";
 import maisBemAvaliados from "./app/home/pages/maisBemAvaliados";
+import Search from "./app/home/pages/Search";
 
 const router = createBrowserRouter([
   {
@@ -29,9 +31,17 @@ const router = createBrowserRouter([
   {
     path: "/mais-bem-avaliados",
     Component: maisBemAvaliados,
-  }
+  },{
+    path: "/search",
+    Component: Search,
+  },
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+
+  return (
+    <Header>
+      <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+    </Header>
+  )
 }
