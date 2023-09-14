4d9a92c5a78d2bc125493433cd307968b4981616
Pedro Barros de Souza Lima <101725415+pbsl-cin@users.noreply.github.com>
2023-09-14 12:09:46 -0300

    postpage style

--- frontend/src/app/home/pages/postPage/index.tsx
+++ frontend/src/app/home/pages/postPage/index.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
+import styles from "./index.module.css";
 //import { UserContext } from "../../context/UserContext";
 import { api } from '../../../../shared/services/ApiService';
 import Comment from '../../components/Comment'
 //import axios from "axios";
 //import { toBase64 } from "../../../../shared/services/base64.js";
-import './postPage.css';
 
 function PostPage() {
     //const { user, setUser, title, setTitle, tags, setTags, body, setBody, image, setImage, comments, setComments } = useContext(PostContext);
@@ -36,8 +36,8 @@ function PostPage() {
                 setTags(response.data.data.tags);
                 setTitle(response.data.data.title);
                 setBody(response.data.data.body);
-                setImgName(response.data.data.image_filename);
-                setImgContent(response.data.data.image_bytes);
+                setImgName(response.data.data.image_name);
+                //setImgContent(response.data.image_content);
                 setComments(response.data.data.comments);
             } catch (error) {
                 console.log('Erro: ' + error);
@@ -46,48 +46,50 @@ function PostPage() {
 
         getPostInfo();
 
-    }, []); // se o [] tiver vazio ele só vai executar uma vez quando abrir
+    }, []); // se o [] tiver vazio ele sÃ³ vai executar uma vez quando abrir
 
     return (
-        <div className="Page">
-            <div className="PostHeader">
-                <div className="user">
-                    <p> {user} </p>
-                </div>
-                <div className="title">
-                    <p> {title} </p>
-                </div>
-                <div className="tags">
-                <p> {tags} </p>
-                </div>
-            </div>
-            <div className="PostBody">
-                <div className="body">
-                <p> {body} </p>
-                </div>
-                {img_name != null && (
-                    <div className="body_image">
-                        <p>[{img_name}]</p>
-                        <img src={img_content} />
+        <section className={styles.container}>
+            <div className={styles.centralize}>
+                <div className={styles.header}>
+                    <div className="user">
+                        <p> user#<b>{user}</b> </p>
                     </div>
-                )}
-                <div>
-                    <button className="CommentButton" onClick={clck_new_comment}>
-                        COMENTAR
-                    </button>
+                    <div className="title">
+                        <p> {title} </p>
+                    </div>
+                    <div className="tags">
+                    <p> {tags} </p>
+                    </div>
+                </div>
+                <div className="PostBody">
+                    <div className="body">
+                    <p> {body} </p>
+                    </div>
+                    {img_name != null && (
+                        <div className="body_image">
+                            <p>[{img_name}]</p>
+                            <img src={img_content} />
+                        </div>
+                    )}
+                    <div>
+                        <button className="CommentButton" onClick={clck_new_comment}>
+                            COMENTAR
+                        </button>
+                    </div>
+                </div>
+                <div style={{width:"100%"}}>
+                {comments.map((comment: Object) => (
+                    <div className={styles.comments} key={comment.id}>
+                        <Comment
+                            user={comment.user}
+                            text={comment.body}
+                        />
+                    </div>
+                ))}
                 </div>
             </div>
-            <div>
-            {comments.map((comment: Object) => (
-                <div className="comments" key={comment.id}>
-                    <Comment
-                        user={comment.user}
-                        text={comment.body}
-                    />
-                </div>
-            ))}
-            </div>
-        </div>
+        </section>
     );
 }
 
